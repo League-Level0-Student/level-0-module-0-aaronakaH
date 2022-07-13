614923192635f330e2bbea2bde4dd1cca803249f
league <league@172.25.112.220>
2022-07-13 16:33:01 -0700

    second

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,42 +10,49 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+		Robot max = new Robot(); max.penDown();
 		// Set the robot speed to 100
-		
+		max.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+		max.setWindowSize(800,800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
+		int count =0;
 		
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+		   while (count < 25) {
+			   
+		  max.setPenColor(0,255,255);
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		   	   max.turn(360/8);
 			   // MOVE           Move the robot 64 pixels
-
+		   	   max.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
-			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+		   	   max.turn(-40);
+			   // DRAW FLAME Move the robot the distance in the variable flameSize
+		   	max.setPenColor(255,255,0);
+		   	   max.move(flameSize);
+		   	
 				//               Turn the robot 170 degrees
-			
+		   	   max.turn(170);
 				//               Move the robot the distance in the variable flameSize (again)
-			
+		   	   max.move(flameSize);
+		   	   max.setPenColor(0,255,255);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+			   max.turn(64);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+			   max.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+			   	count +=1;
 		//  End the while loop here
-		
+		}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
 		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
 		//        Run the program again. Check the second picture in the recipe.
-
+		
+		
 		// LOOP   When you have one arm looking right, change your loop to repeat 25 times and run the program.
 		
 	}
