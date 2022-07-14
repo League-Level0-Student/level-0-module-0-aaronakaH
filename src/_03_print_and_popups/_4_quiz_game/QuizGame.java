19f00303835a8e664917fff1515e59c338aadff2
league <league@172.25.112.220>
2022-07-14 16:30:53 -0700

    i worked on java

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,39 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score =0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String input = JOptionPane.showInputDialog("What was the name of the first president?");
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
+				if(input.equalsIgnoreCase("George washinton")) {
+					score+=1;
+				}
 				
-				// 4.  if the user's answer was correct, add one to their score 
+				
 		
+		//  ASK A QUESTION AND CHECK THE ANSWER
+				String B = JOptionPane.showInputDialog("What color is the sky?");		
+				//     Use an if statement to check if their answer is correct
+				if(B.equalsIgnoreCase("blue")) {
+					score +=1;
+				}
+				
+				
+			//  ASK A QUESTION AND CHECK THE ANSWER
+			String n = JOptionPane.showInputDialog("Am i 12?");	
+				// 3.  Use an if statement to check if their answer is correct
+				if(n.equalsIgnoreCase("no")) {
+					
+				}
+				// 4.  if the user's answer was correct, add one to their score 
+				
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
