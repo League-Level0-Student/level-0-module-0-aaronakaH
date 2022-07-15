eab0ca52714ed0f56d56665f9d80e2b3ea755fcf
league <league@172.25.112.220>
2022-07-15 16:56:53 -0700

    java

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -22,7 +22,7 @@ public class PentagonCrazy {
 		sam.setPenColor(0,0,255);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-		int sides =5;
+		int sides =8;
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
 		int angle = 360/sides;
